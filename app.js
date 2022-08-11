0c37a5e1647a10e7304e8dae7c7d50dfb7c508d5
Oleg Dmitriev <oleg_surgut@hotmail.com>
2022-08-11 11:40:39 +0500

    fix4

--- app.js
+++ app.js
@@ -26,8 +26,8 @@ app.use((req, res, next) => {
 app.use('/users', require('./routes/users'));
 app.use('/cards', require('./routes/cards'));
 
-app.use((err, req, res) => {
-  res.status(404).send({ message: 'Страница не найдена' });
+app.all('*', (req, res, next) => {
+  next(res.status(404).send({ message: 'Неправильный путь' }));
 });
 
 app.listen(PORT, () => {
