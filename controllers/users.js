25edde61c615e297975c1acc74278ae9aa6276ed
Oleg Dmitriev <oleg_surgut@hotmail.com>
2022-08-15 10:36:11 +0500

    fix8.3

--- controllers/users.js
+++ controllers/users.js
@@ -13,7 +13,7 @@ module.exports.findUser = (req, res) => {
       if (!user) { res.status(404).send({ message: 'Нет пользователя с таким id' }); } else res.send({ data: user });
     })
     .catch((err) => {
-      if (err.name === 'ValidationError') {
+      if (err.kind === 'objectid') {
         res.status(400).send({ message: 'Переданы некорректные данные' });
       } else {
         res.status(500).send({ message: 'Ошибка сервера' });
