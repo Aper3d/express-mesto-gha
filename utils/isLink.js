2e8597b6b4bf84d4e6a2b08029c5570acf93138e
Oleg Dmitriev <92028362+Aper3d@users.noreply.github.com>
2022-08-19 16:33:27 +0500

    Update isLink.js

--- utils/isLink.js
+++ utils/isLink.js
@@ -1,4 +1,4 @@
 const RegExp = /^((http|https):\/\/)?(www\.)?([A-Za-z0-9]{1}[A-Za-z0-9-]*\.?)*\.{1}[A-Za-z0-9-]{2,8}(\/([\w#!:.?+=&%@!\-/])*)?/;
 const isLink = (link) => RegExp.test(link);
 
-module.exports = { isLink, reg };
+module.exports = { isLink, RegExp };
