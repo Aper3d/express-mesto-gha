c4d311f8c6d772b472136b1cdb9bb4f326131575
Oleg Dmitriev <oleg_surgut@hotmail.com>
2022-08-22 21:14:15 +0500

    pr14 r1fix1

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,22 +4,18 @@ const UnauthorizedError = require('../errors/UnauthorizedError');
 
 const { JWT_SECRET = 'JWT_SECRET' } = process.env;
 
-const handleAuthError = () => {
-  throw new UnauthorizedError('Необходима авторизация');
-};
-
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    handleAuthError(next);
+    next(new UnauthorizedError('Необходима авторизация'));
   }
   const token = authorization.replace('Bearer ', '');
   let payload;
   try {
     payload = jwt.verify(token, JWT_SECRET);
   } catch (err) {
-    handleAuthError(next);
+    return next(new UnauthorizedError('Необходима авторизация'));
   }
   req.user = payload;
-  next();
+  return next();
 };
